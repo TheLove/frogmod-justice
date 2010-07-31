154b9b6ea901e70b1278d4f0b0d459d30625e680
Mihai Draghicioiu <mihai.draghicioiu@gmail.com>
2010-07-31 03:25:24 +0300

    Updated html + js:
    now compatible with IE8 and FF
    added spec/unspec link in admin
    added Info section
    other cleanups/fixes

--- web/frogmod.js
+++ web/frogmod.js
@@ -1,5 +1,10 @@
+// frogmod.js
+
+var info_update_millis = 100000; // server info rarely changes
 var players_update_millis = 2000;
 var status_update_millis = 2000;
+var admin = false;
+var states = [ 'Alive', 'Dead', 'Spawning', 'Lagged', 'Editing', 'Spectator' ];
 
 function ajaxCall(uri, cb) {
 	var xhr;
@@ -9,7 +14,7 @@ function ajaxCall(uri, cb) {
 		xhr=new ActiveXObject("Microsoft.XMLHTTP");
 	}
 	xhr.onreadystatechange = cb;
-	xhr.open('GET', uri, true);
+	xhr.open('GET', uri + '?' + Date(), true);
 	xhr.send();
 }
 
@@ -24,52 +29,99 @@ function format_time(t) {
 	return (days > 0 ? days + 'd ': '') + hours + ':' + minutes + ':' + seconds;
 }
 
-function update_players() {
-	ajaxCall('/players', function() {
-		if(this.readyState == 4 && this.status == 200) {
-			var div = document.getElementById('players');
+function update_info() {
+	ajaxCall('/info', function() {
+		if(this.readyState == 4) {
+			var div = document.getElementById('info');
 			if(div) {
-				var players = eval('(' + this.responseText + ')');
-				if(players && players.length > 0) {
-					var html = new Array();
-					html.push('<table><tr><th>Cn</th><th>Name</th><th>Team</th><th>Ping</th><th>Uptime</th></tr>');
-					for(p in players) {
-						html.push('<tr class="privilege'+players[p].privilege+'"><td>'+players[p].clientnum+'</td><td>'+players[p].name+'</td><td>'+players[p].team+'</td><td>'+players[p].ping+'</td><td>'+format_time(players[p].connectmillis)+'</td></tr>');
-					}
-					html.push('</table>');
-					div.innerHTML = html.join('');
-				} else div.innerHTML = 'No players on the server.';
+				if(this.status == 200) {
+					var st = eval('('+this.responseText+')');
+					if(st) {
+						var h1 = document.getElementById('title');
+						if(h1 && st.serverdesc && st.serverdesc != '') h1.innerHTML = st.serverdesc + (admin?' admin':'') + ' console';
+
+						var html = new Array();
+						html.push('<ul>');
+						html.push('<li>Server name: '+st.serverdesc+'</li>');
+						html.push('<li>Message of the day: '+st.servermotd+'</li>');
+						html.push('<li>Max players: '+st.maxclients+'</li>');
+						html.push('</ul>');
+						div.innerHTML = html.join('');
+					} else div.innerHTML = 'Could not parse response';
+				} else div.innerHTML = 'Error ' + this.status;
+				setTimeout(update_status, status_update_millis);
 			}
-			setTimeout(update_players, players_update_millis);
 		}
 	});
 }
 
 function update_status() {
 	ajaxCall('/status', function() {
-		if(this.readyState == 4 && this.status == 200) {
+		if(this.readyState == 4) {
 			var div = document.getElementById('status');
 			if(div) {
-				var st = eval('('+this.responseText+')');
-				if(st) {
-					var html = new Array();
-					html.push('<ul>');
-					html.push('<li>Uptime: '+format_time(st.totalmillis)+'</li>');
-					html.push('<li>Mastermode: '+st.mastermodename+' ('+st.mastermode+')</li>');
-					html.push('<li>Game mode: '+st.gamemodename+' ('+st.gamemode+')</li>');
-					html.push('<li>Map: '+(st.map?'<b>'+st.map+'</b>':'<i>No map</i>')+'</li>');
-					html.push('<li>Max players: '+st.maxclients+'</li>');
-					html.push('</ul>');
-					div.innerHTML = html.join('');
-				} else div.innerHTML = 'No players on the server.';
+				if(this.status == 200) {
+					var st = eval('('+this.responseText+')');
+					if(st) {
+						var html = new Array();
+						html.push('<ul>');
+						html.push('<li>Uptime: '+format_time(st.totalmillis)+'</li>');
+						html.push('<li>Mastermode: '+st.mastermodename+' ('+st.mastermode+')</li>');
+						html.push('<li>Game mode: '+st.gamemodename+' ('+st.gamemode+')</li>');
+						html.push('<li>Map: '+(st.map?'<b>'+st.map+'</b>':'<i>No map</i>')+'</li>');
+						html.push('</ul>');
+						div.innerHTML = html.join('');
+					} else div.innerHTML = 'No players on the server.';
+				} else div.innerHTML = 'Error ' + this.status
+				setTimeout(update_status, status_update_millis);
 			}
-			setTimeout(update_status, status_update_millis);
 		}
 	});
 }
 
-function init() {
-	update_players();
-	update_status();
+
+function update_players_cb(xhr) {
+	if(xhr.readyState == 4) {
+		var div = document.getElementById('players');
+		if(div) {
+			if(xhr.status == 200) {
+				var players = eval('(' + xhr.responseText + ')');
+				if(players && players.length > 0) {
+					var html = new Array();
+					html.push('<table><tr><th>Cn</th><th>Name</th><th>Team</th><th>Status</th><th>Ping</th><th>Uptime</th>'+(admin?'<th>Actions</th>':'')+'</tr>');
+					for(p in players) {
+						html.push('<tr class="privilege'+players[p].privilege+'">');
+						html.push('<td>'+players[p].clientnum+'</td>');
+						html.push('<td>'+players[p].name+'</td>');
+						html.push('<td>'+players[p].team+'</td>');
+						html.push('<td>'+states[players[p].state]+'</td>');
+						html.push('<td>'+players[p].ping+'</td>');
+						html.push('<td>'+format_time(players[p].connectmillis)+'</td>');
+						if(admin) {
+							html.push('<td><a href="?kick='+players[p].clientnum+'" title="Kick">[K]</a> ');
+							if(players[p].state == 5)
+								html.push('<a href="?unspec='+players[p].clientnum+'" title="Unspec">[U]</a> ');
+							else
+								html.push('<a href="?spec='+players[p].clientnum+'" title="Spec">[S]</a> ');
+							html.push('</td>');
+						}
+						html.push('</tr>');
+					}
+					html.push('</table>');
+					div.innerHTML = html.join('');
+				} else div.innerHTML = 'No players on the server.';
+			} else div.innerHTML = 'Error ' + xhr.status;
+			setTimeout(update_players, players_update_millis);
+		}
+	}
 }
 
+function update_players(admin) {
+	ajaxCall('/players', admin ? function() { update_players_cb(this, true); } : function() { update_players_cb(this, false); });
+}
+
+function init() {
+	update_info();
+	update_status();
+	update_players();
+}
