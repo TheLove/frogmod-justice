c7d2cbde596d0e8548798668d10e1a5f6fdb9c06
Mihai Draghicioiu <mihai.draghicioiu@gmail.com>
2010-11-13 16:56:52 +0200

    added run-debug.sh

---
+++ run-debug.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ulimit -c unlimited
+
+while true; do
+echo 'Starting ./frogserv'
+./frogserv
+mv core core-`date +"%F_%H-%M-%S"`
+if [ ! -z $1 ]; then
+/usr/sbin/sendmail -t "$1" << EOF
+From: frogserv@mappinghell.net
+Subject: Server crash
+
+Server crashed.
+
+.
+
+
+EOF
+
+echo 'Crash email sent'
+fi
+done
