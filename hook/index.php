c763cc629a0e87d313af4c3180f7d0dcf8a6a24a
Mihai <mihai@lappy.(none)>
2011-02-19 21:32:48 +0000

    added recent disconnects list to hook index

--- hook/index.php
+++ hook/index.php
@@ -18,7 +18,23 @@ if (isset($argv[1]) && $argv[1]) {
 } elseif($_SERVER['REQUEST_METHOD'] == 'POST') {
     $jsonString = trim(file_get_contents('php://input'));
 } else { ?>
-<h1>Frogmod hook</h1>
+<h1>Frogmod database</h1>
+<?
+try {
+$pdo = Database::getPDO();
+$lastPlayers = $pdo->query("SELECT * FROM disconnects, players WHERE players.id = disconnects.player_id ORDER BY disconnects.timestamp DESC LIMIT 20");
+} catch(PDOException $e) {
+	echo $e;
+}
+?>
+<h2>Disconnects</h2>
+<table border="1">
+<tr><th>Time</th><th>Name</th><th>IP</th><th>Duration</th></tr>
+<? foreach($lastPlayers as $p) { ?>
+	<tr><td><?=date('d.m.Y H:i:s', strtotime($p['timestamp']))?></td><td><?=htmlspecialchars($p['name'])?></td><td><?=$p['ip_address']?></td><td><? printf("%02d:%02d", floor($p['connection_time']/3600000), floor($p['connection_time']/60000)) ?></td></tr>
+<? } ?>
+</table>
+
 <?
 }
 
